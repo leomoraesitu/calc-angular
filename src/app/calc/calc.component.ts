e49a791e2992dc47e460b5c2f45ca07880ceb6a1
leomoraesitu <leomoraesitu@hotmail.com>
2023-08-22 08:59:15 -0300

    Add Operações

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -8,6 +8,23 @@ import { Component } from '@angular/core';
 export class CalcComponent {
   num1: number=0;
   num2: number=0;
-  soma: number=0;
+  resultado: number=0;
+
+somar(){
+  this.resultado = this.num1 + this.num2;
+}
+
+subtrair(){
+  this.resultado = this.num1 - this.num2;
+}
+
+multiplicar(){
+  this.resultado = this.num1 * this.num2;
+}
+
+dividir(){
+  this.resultado = this.num1 / this.num2;
+}
+
 }
 
